3530a0c0e9b2a938b3f7f2599ab7fd63c52fd5e0
Jay Valentine <jayv136@gmail.com>
2022-06-24 15:11:08 +0100

    Added skeleton implementation of list function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::{env, process};
+
+// Function to print invalid usage command
+// and inform the user of available commands.
+fn invalid_usage() -> ! {
+    println!("Usage:");
+    println!("    todo <command>");
+    println!("");
+    println!("Available commands:");
+    println!("    list");
+
+    process::exit(1);
+}
+
+fn do_list<T: Iterator<Item = String>>(project: Vec<&str>, args: T) {
+    for s in project {
+        println!("{}", s);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let project: Vec<&str> = vec![];
+
+    // Skip first argument - this will be the path to the executable.
+    let mut args = env::args().skip(1);
+
+    // We need at least one argument - this is the command.
+    let command = match args.next() {
+        Some(s) => s,
+        None => invalid_usage()
+    };
+
+    println!("{}", command);
+
+    // Match the command and perform the right function.
+    do_list(project, args);
 }
