440d40d61b322d29880de740f7887c7761ef5a68
Jay Valentine <jayv136@gmail.com>
2022-06-24 15:49:17 +0100

    Implemented list and new functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,51 @@
-use std::{env, process};
+use std::{env, process, fs::File};
+use std::io::{prelude::*, BufReader, LineWriter};
+use core::str::FromStr;
+
+enum TaskState {
+    Open,
+    Complete
+}
+
+// A task with a state and a unique identifier.
+struct Task {
+    // The state of the task - open, closed, incomplete, etc.
+    state: TaskState,
+
+    // A description of the task.
+    description: String
+}
+
+enum TaskParseError {
+    InvalidState,
+    NoState
+}
+
+impl FromStr for Task {
+    type Err = TaskParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // Split on whitespace.
+        let mut split = s.split_whitespace();
+        let state = match split.next() {
+            Some(i) => i,
+            None => return Err(TaskParseError::NoState)
+        };
+
+        let state = match state {
+            "?" => TaskState::Open,
+            "/" => TaskState::Complete,
+            _ => return Err(TaskParseError::InvalidState)
+        };
+
+        let description: Vec<&str> = split.collect();
+        let description: String = description.join(" ");
+
+        return Ok(Task { state, description });
+    }
+}
+
+struct Project(Vec<Task>);
 
 // Function to print invalid usage command
 // and inform the user of available commands.
@@ -12,14 +59,51 @@ fn invalid_usage() -> ! {
     process::exit(1);
 }
 
-fn do_list<T: Iterator<Item = String>>(project: Vec<&str>, args: T) {
-    for s in project {
-        println!("{}", s);
+fn do_list<T: Iterator<Item = String>>(project: &mut Project, args: T) {
+    for s in &project.0 {
+        println!("{}", s.description);
     }
 }
 
+fn do_new<T: Iterator<Item = String>>(project: &mut Project, args: T) {
+    let description: Vec<String> = args.collect();
+    let description = description.join(" ");
+
+    let state = TaskState::Open;
+
+    project.0.push(Task { state, description });
+}
+
+fn open_project() -> Project {
+    // Open the project file.
+    // Create the project file if it doesn't already exist.
+    let project_file = match File::open(".todo") {
+        Ok(f) => f,
+        Err(_) => {
+            {
+                println!("Creating new project file");
+                File::create(".todo").expect("Could not create new project file!");
+            }
+            File::open(".todo").expect("Could not open newly-created project file!")
+        }
+    };
+
+    let reader = BufReader::new(project_file);
+    let mut project = vec![];
+
+    for line in reader.lines() {
+        let line = line.expect("Invalid line in project!");
+
+        if let Ok(v) = Task::from_str(line.trim()) {
+            project.push(v);
+        }
+    }
+
+    return Project(project);
+}
+
 fn main() {
-    let project: Vec<&str> = vec![];
+    let mut project: Project = open_project();
 
     // Skip first argument - this will be the path to the executable.
     let mut args = env::args().skip(1);
@@ -30,8 +114,26 @@ fn main() {
         None => invalid_usage()
     };
 
-    println!("{}", command);
-
     // Match the command and perform the right function.
-    do_list(project, args);
+    match command.as_str() {
+        "list" => do_list(&mut project, args),
+        "new" => do_new(&mut project, args),
+        _ => invalid_usage()
+    };
+
+    // Write back the project to the file.
+    let f = File::create(".todo").expect("Could not open project file!");
+    let mut w = LineWriter::new(f);
+
+    for t in project.0 {
+        let state = match t.state {
+            TaskState::Complete => "/",
+            TaskState::Open => "?"
+        };
+
+        w.write(state.as_bytes()).expect("Could not write task to project file!");
+        w.write(" ".as_bytes()).expect("Could not write task to project file!");
+        w.write(t.description.as_bytes()).expect("Could not write task to project file!");
+        w.write("\n".as_bytes()).expect("Could not write task to project file!");
+    }
 }
